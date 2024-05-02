9ee54ad0c3505238ce5ce161bf52604882afdc54
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-02 12:10:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+  let oldtable=document.getElementById("sampleTable");
+  let prevHTML=oldtable.innerHTML;
+	oldtable.innerHTML="";
+	oldtable.innerHTML+=`<tr><td value="New Cell1">New Cell1</td> 
+		<td value="New Cell2">New Cell2</td></tr>`+prevHTML;
+		
   
 }
